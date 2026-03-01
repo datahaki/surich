090377ec02738df0b7d646c6ab4166c1150508a6
datahaki <jan.hakenberg@gmail.com>
2026-03-01 18:32:57 +0100

    run provider test

--- src/test/java/ch/alpine/surich/RunProviderTest.java
+++ src/test/java/ch/alpine/surich/RunProviderTest.java
@@ -5,7 +5,6 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
-import org.junit.jupiter.api.TestReporter;
 
 import ch.alpine.bridge.cgr.InstanceDiscovery;
 import ch.alpine.bridge.pro.RunProvider;
@@ -13,12 +12,9 @@ import ch.alpine.bridge.pro.SanityCheckRunProvider;
 
 class RunProviderTest {
   @TestFactory
-  Stream<DynamicTest> dynamicTests(TestReporter testReporter) {
+  Stream<DynamicTest> dynamicTests() {
     return InstanceDiscovery.of(getClass().getPackageName(), RunProvider.class).stream() //
         .map(instanceRecord -> DynamicTest.dynamicTest(instanceRecord.toString(), //
-            () -> {
-              testReporter.publishEntry("=="+instanceRecord.toString());
-              SanityCheckRunProvider.INSTANCE.accept(instanceRecord);
-            }));
+            () -> SanityCheckRunProvider.INSTANCE.accept(instanceRecord)));
   }
 }
